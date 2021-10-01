78cf9f7151cdeb1c92342ba18dec014dbbe3aeae
tecnovert <tecnovert@tecnovert.net>
2021-10-01 20:37:28 +0200

    consensus: Prepare second RCT whitelist for next hardfork.

--- src/wallet/hdwallet.cpp
+++ src/wallet/hdwallet.cpp
@@ -11792,6 +11792,8 @@ void CHDWallet::AvailableAnonCoins(interfaces::Chain::Lock& locked_chain, std::v
     const bool spend_frozen = {coinControl ? coinControl->m_spend_frozen_blinded : false};
     const bool include_tainted_frozen = {coinControl ? coinControl->m_include_tainted_frozen : false};
 
+    int64_t time_now = GetAdjustedTime();
+
     CHDWalletDB wdb(*database, "r");
 
     const Consensus::Params &consensusParams = Params().GetConsensus();
@@ -11862,7 +11864,7 @@ void CHDWallet::AvailableAnonCoins(interfaces::Chain::Lock& locked_chain, std::v
                     !stx.tx->vpout[r.n]->IsType(OUTPUT_RINGCT) ||
                     !pblocktree->ReadRCTOutputLink(((CTxOutRingCT*)stx.tx->vpout[r.n].get())->pk, index) ||
                     IsBlacklistedAnonOutput(index) ||
-                    (!IsWhitelistedAnonOutput(index) && r.nValue > consensusParams.m_max_tainted_value_out)) {
+                    (!IsWhitelistedAnonOutput(index, time_now, consensusParams) && r.nValue > consensusParams.m_max_tainted_value_out)) {
                     continue;
                 }
             }
