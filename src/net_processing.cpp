8aaf0a4ce98b33861924c062064d99be4292d0e9
tecnovert <tecnovert@tecnovert.net>
2021-10-05 20:36:56 +0200

    net: Ignore smsg for peers connected as ADDR_FETCH or FEELER.

--- src/net_processing.cpp
+++ src/net_processing.cpp
@@ -4343,7 +4343,9 @@ void PeerManagerImpl::ProcessMessage(CNode& pfrom, const std::string& msg_type, 
         return;
     }
 
-    if (smsg::SMSG_UNKNOWN_MESSAGE != smsgModule.ReceiveData(this, &pfrom, msg_type, vRecv)) {
+    if (!(pfrom.IsAddrFetchConn() || pfrom.IsFeelerConn()) &&
+        smsg::SMSG_UNKNOWN_MESSAGE != smsgModule.ReceiveData(this, &pfrom, msg_type, vRecv)) {
+        // If smsg::fSecMsgEnabled is false smsgModule.ReceiveData will ignore SMSGMsgType::PING messages to avoid the Unknown command message
         return;
     }
 
@@ -5294,7 +5296,8 @@ bool PeerManagerImpl::SendMessages(CNode* pto)
         MaybeSendFeefilter(*pto, current_time);
     } // release cs_main
 
-    if (smsg::fSecMsgEnabled) {
+    if (smsg::fSecMsgEnabled &&
+        !(pto->IsAddrFetchConn() || pto->IsFeelerConn())) {
         bool fSendTrickle = pto->HasPermission(NetPermissionFlags::NoBan);
         smsgModule.SendData(pto, fSendTrickle);
     }
