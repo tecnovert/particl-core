9660d3daf8d823007a1573a41c3d703671496adf
tecnovert <tecnovert@tecnovert.net>
2022-06-20 15:36:12 +0200

    tests: Test listsinceblock in Particl mode

--- src/wallet/rpc/transactions.cpp
+++ src/wallet/rpc/transactions.cpp
@@ -764,6 +764,10 @@ RPCHelpMan listtransactions()
                         {
                             {RPCResult::Type::BOOL, "involvesWatchonly", /*optional=*/true, "Only returns true if imported addresses were involved in transaction."},
                             {RPCResult::Type::STR, "address", /*optional=*/true, "The particl address of the transaction."},
+                            {RPCResult::Type::STR, "account", /*optional=*/true, "Alias of label."},
+                            {RPCResult::Type::STR, "stealth_address", /*optional=*/true, "The stealth address the transaction was received on."},
+                            {RPCResult::Type::STR, "coldstake_address", /*optional=*/true, "The address the transaction is staking on."},
+                            {RPCResult::Type::STR, "type", /*optional=*/true, "anon/blind/standard."},
                             {RPCResult::Type::STR, "category", "The transaction category.\n"
                                 "\"send\"                  Transactions sent.\n"
                                 "\"receive\"               Non-coinbase transactions received.\n"
@@ -916,6 +920,10 @@ RPCHelpMan listsinceblock()
                             {
                                 {RPCResult::Type::BOOL, "involvesWatchonly", /*optional=*/true, "Only returns true if imported addresses were involved in transaction."},
                                 {RPCResult::Type::STR, "address", /*optional=*/true, "The particl address of the transaction."},
+                                {RPCResult::Type::STR, "account", /*optional=*/true, "Alias of label."},
+                                {RPCResult::Type::STR, "stealth_address", /*optional=*/true, "The stealth address the transaction was received on."},
+                                {RPCResult::Type::STR, "coldstake_address", /*optional=*/true, "The address the transaction is staking on."},
+                                {RPCResult::Type::STR, "type", /*optional=*/true, "anon/blind/standard."},
                                 {RPCResult::Type::STR, "category", "The transaction category.\n"
                                     "\"send\"                  Transactions sent.\n"
                                     "\"receive\"               Non-coinbase transactions received.\n"
@@ -927,6 +935,7 @@ RPCHelpMan listsinceblock()
                                 {RPCResult::Type::NUM, "vout", "the vout value"},
                                 {RPCResult::Type::STR_AMOUNT, "fee", /*optional=*/true, "The amount of the fee in " + CURRENCY_UNIT + ". This is negative and only available for the\n"
                                      "'send' category of transactions."},
+                                {RPCResult::Type::STR_AMOUNT, "reward", /*optional=*/true, "The reward if the transaction is a coinstake."},
                             },
                             TransactionDescriptionString()),
                             {
