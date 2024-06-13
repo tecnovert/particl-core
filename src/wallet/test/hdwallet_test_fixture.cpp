1b0ca8ddceeb9c15497fefb4a5dabd3246b45df1
tecnovert <tecnovert@tecnovert.net>
2024-06-13 17:25:48 +0200

    wallet: Disable missing account warning in benchmarks.

--- src/wallet/test/hdwallet_test_fixture.cpp
+++ src/wallet/test/hdwallet_test_fixture.cpp
@@ -24,6 +24,7 @@ HDWalletTestingSetup::HDWalletTestingSetup(const ChainType chain_type):
     m_wallet_loader{interfaces::MakeWalletLoader(*m_node.chain, *Assert(m_node.args))}
 {
     pwalletMain = std::make_shared<CHDWallet>(m_node.chain.get(), "", CreateMockWalletDatabaseBDB());
+    pwalletMain->m_warn_no_active_acc = false;
     WalletContext& wallet_context = *m_wallet_loader->context();
     AddWallet(wallet_context, pwalletMain);
     pwalletMain->LoadWallet();
