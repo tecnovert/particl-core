77357defdb12a258eb0bbfb698e7a6d85b298ba8
tecnovert <tecnovert@tecnovert.net>
2025-07-07 18:39:47 +0200

    rpc: Add pubkey_from option to smsginbox.

--- src/smsg/rpcsmessage.cpp
+++ src/smsg/rpcsmessage.cpp
@@ -1463,6 +1463,7 @@ static RPCHelpMan smsginbox()
                             {"offset", RPCArg::Type::NUM, RPCArg::Default{""}, "Skip the first \"offset\" messages"},
                             {"max_results", RPCArg::Type::NUM, RPCArg::Default{""}, "Return only \"max_results\" messages"},
                             {"unread_only", RPCArg::Type::BOOL, RPCArg::Default{false}, "Count only unread messages"},
+                            {"pubkey_from", RPCArg::Type::BOOL, RPCArg::Default{false}, "Display the public key the message was sent from (if possible)."},
                         },
                         "options"},
                 },
@@ -1519,6 +1520,7 @@ static RPCHelpMan smsginbox()
     std::string sEnc = "text";
     bool update_status = true;
     bool unread_only = false;
+    bool pubkey_from = false;
     int offset = 0, max_results = -1;
     if (request.params[2].isObject()) {
         UniValue options = request.params[2].get_obj();
@@ -1528,15 +1530,19 @@ static RPCHelpMan smsginbox()
         if (options["encoding"].isStr()) {
             sEnc = options["encoding"].get_str();
         }
-        if (options["unread_only"].isBool()) {
-            unread_only = options["unread_only"].get_bool();
-        }
         if (options["offset"].isNum()) {
             offset = options["offset"].get_int();
         }
         if (options["max_results"].isNum()) {
             max_results = options["max_results"].get_int();
         }
+        if (options["unread_only"].isBool()) {
+            unread_only = options["unread_only"].get_bool();
+        }
+        if (options["pubkey_from"].isBool()) {
+            pubkey_from = options["pubkey_from"].get_bool();
+        }
+
     }
 
     UniValue result(UniValue::VOBJ);
@@ -1581,8 +1587,8 @@ static RPCHelpMan smsginbox()
 
             result.pushKV("result", strprintf("Deleted %u messages.", nMessages));
         } else
-        if (mode == "all"
-            || mode == "unread") {
+        if (mode == "all" ||
+            mode == "unread") {
             int fCheckReadStatus = mode == "unread" ? 1 : 0;
 
             smsg::SecMsgStored smsgStored;
@@ -1613,8 +1619,10 @@ static RPCHelpMan smsginbox()
                 objM.pushKV("msgid", HexStr(Span<const unsigned char>(&chKey[2], 28))); // timestamp+hash
                 objM.pushKV("version", strprintf("%02x%02x", psmsg->version[0], psmsg->version[1]));
 
+                CPubKey pk_from_out;
+                CPubKey *ppk = pubkey_from ? &pk_from_out : nullptr;
                 uint32_t nPayload = smsgStored.vchMessage.size() - smsg::SMSG_HDR_LEN;
-                int rv = smsgModule.Decrypt(false, smsgStored.addrTo, pHeader, pHeader + smsg::SMSG_HDR_LEN, nPayload, msg);
+                int rv = smsgModule.Decrypt(false, smsgStored.addrTo, pHeader, pHeader + smsg::SMSG_HDR_LEN, nPayload, msg, ppk);
                 if (rv == 0) {
                     std::string sAddrTo = EncodeDestination(PKHash(smsgStored.addrTo));
                     std::string sText = GetMessageText(msg);
@@ -1639,6 +1647,10 @@ static RPCHelpMan smsginbox()
                     objM.pushKV("payloadsize", (int)nPayload);
 
                     objM.pushKV("from", msg.sFromAddress);
+                    if (pk_from_out.IsValid()) {
+                        objM.pushKV("pubkey_from", HexStr(pk_from_out));
+                    }
+
                     objM.pushKV("to", sAddrTo);
                     if (sEnc == "none") {
                     } else
@@ -2351,7 +2363,7 @@ static RPCHelpMan smsgone()
                             {"pubkey_from", RPCArg::Type::BOOL, RPCArg::Default{false}, "Display the public key the message was sent from (if possible)."},
                         },
                         "options"},
-                },
+               },
                 RPCResult{
                     RPCResult::Type::OBJ, "", "", {
                         {RPCResult::Type::STR_HEX, "msgid", "Message id"},
